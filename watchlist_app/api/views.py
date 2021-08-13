e440236189c0352b4a2940b86d2ae62a76400429
Nilesh Agarwal <eternalshenron@gmail.com>
2021-08-13 11:05:08 +0530

    5.2

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,18 +1,42 @@
 from rest_framework import request
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 from ..models import Movie
 from .serializers import MovieSerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movies_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if (request.method == 'GET'):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    if (request.method == 'POST'):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, pk):
     movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+
+    if (request.method == 'GET'):
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    if (request.method == 'PUT'):
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else: 
+            return Response(serializer.errors)
+
+    if (request.method == 'DELETE'):
+        movie.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
